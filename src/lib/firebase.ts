1f86e99507a832f740873b22e3d707561382ec98
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-08-11 10:38:37 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp, FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -13,7 +14,7 @@ const firebaseConfig: FirebaseOptions = {
   "projectId": "creditwise-ai-nd7s0",
   "appId": "1:492248595221:web:ba48c35eec8dc524a6071c",
   "storageBucket": "creditwise-ai-nd7s0.appspot.com",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "apiKey": "AIzaSyAeIyY3IH-zAkV8FSmmKyR1b32pCa46fQg",
   "authDomain": "creditwise-ai-nd7s0.firebaseapp.com",
   "messagingSenderId": "492248595221"
 };
