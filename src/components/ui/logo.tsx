0478f11d22c8ab559c8f348ddaf898621682b897
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-08-12 17:13:23 +0000

    Warning: Invalid DOM property `stroke-width`. Did you mean `strokeWidth`

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -10,8 +10,8 @@ export const Logo = ({ className }: { className?: string }) => (
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
     >
-        <path d="M16.498 6.50009H6.49805C4.28891 6.50009 2.49805 8.29097 2.49805 10.5001V16.5001C2.49805 18.7092 4.28891 20.5001 6.49805 20.5001H14.498C16.7072 20.5001 18.498 18.7092 18.498 16.5001V10.5001C18.498 8.29097 16.7072 6.50009 14.498 6.50009H16.498Z" stroke="hsl(var(--primary))" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M7 13.5L10 16.5L15 11.5" stroke="hsl(var(--primary))" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M16.498 6.50009H6.49805C4.28891 6.50009 2.49805 8.29097 2.49805 10.5001V16.5001C2.49805 18.7092 4.28891 20.5001 6.49805 20.5001H14.498C16.7072 20.5001 18.498 18.7092 18.498 16.5001V10.5001C18.498 8.29097 16.7072 6.50009 14.498 6.50009H16.498Z" stroke="hsl(var(--primary))" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M7 13.5L10 16.5L15 11.5" stroke="hsl(var(--primary))" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
         <circle cx="18.5" cy="5.5" r="2.5" fill="hsl(var(--primary))"/>
     </svg>
 
