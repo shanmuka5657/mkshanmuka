34ed2f4b1c57d2b3e65749326bfa075c8acbfcc1
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-08-15 14:07:02 +0000

    so we dont have any problem with this , where is dashboard

--- src/app/api/auth/session/route.ts
+++
@@ -1,3 +0,0 @@
-
-// This file is no longer needed and can be removed.
-// The new authentication flow does not require this server-side session management.
--- src/lib/firestore-service.ts
+++
@@ -1,3 +0,0 @@
-
-// This file is no longer in use since authentication has been removed.
-// It can be deleted.
