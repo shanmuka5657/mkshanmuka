2d807d2c762be733f43dab45970d49fc67f9e8c0
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-08-15 12:59:21 +0000

    Executing task: xvfb-run npx -y lighthouse "http://0.0.0.0:9000/"     --

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/studiopaper-assets/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
